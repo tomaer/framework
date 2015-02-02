80ed2711e06b8ec37fbac9ad6a919572bb7e81f9
tomaer <i@tomaer.com>
2015-02-02 21:49:45 +0800

    提交分页相关工具

---
+++ framework-common/src/main/java/com/tomaer/framework/common/paging/Paging.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2009 Tomaer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.tomaer.framework.common.paging;
+
+import java.io.Serializable;
+
+/**
+ * Description:分页对象
+ * Author: tomaer
+ * Version: 1.0
+ * Date: 2015/2/2 21:43
+ */
+public class Paging implements Serializable {
+
+    private static final long serialVersionUID = 9028264571706638217L;
+}
