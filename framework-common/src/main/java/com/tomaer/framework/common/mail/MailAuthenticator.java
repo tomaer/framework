c5102a4a46732ee52dfc892a92987d50a16e7ded
tomaer <i@tomaer.com>
2015-02-02 15:53:33 +0800

    提交代码

---
+++ framework-common/src/main/java/com/tomaer/framework/common/mail/MailAuthenticator.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2009 Tomaer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.tomaer.framework.common.mail;
+
+import java.io.Serializable;
+
+/**
+ * Description:
+ * Author: tomaer
+ * Version: 1.0
+ * Date: 2015/2/2 15:29
+ */
+public class MailAuthenticator implements Serializable {
+
+    private static final long serialVersionUID = 4177380060749627922L;
+}
