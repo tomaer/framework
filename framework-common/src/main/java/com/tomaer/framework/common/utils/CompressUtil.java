879e632f1c0705463f1e4c98de26ff422408f577
tomaer <i@tomaer.com>
2015-02-05 13:26:42 +0800

    提交文件压缩解压缩工具类

--- framework-common/src/main/java/com/tomaer/framework/common/utils/CompressUtil.java
+++ framework-common/src/main/java/com/tomaer/framework/common/utils/CompressUtil.java
@@ -15,6 +15,12 @@
  */
 package com.tomaer.framework.common.utils;
 
+
+import com.tomaer.framework.common.logger.Logger;
+import com.tomaer.framework.common.logger.LoggerFactory;
+import org.apache.commons.lang3.StringUtils;
+
+
 /**
  * Description:文件压缩,解压缩处理,底层使用Apache commons-compress
  * Author: tomaer
@@ -23,12 +29,28 @@ package com.tomaer.framework.common.utils;
  */
 public class CompressUtil {
     
+    static final Logger logger = LoggerFactory.getLogger(CompressUtil.class);
+
+    /**
+     * 压缩直接路径的文件为zip文件,并且放到指定位置
+     * @param source 源文件路径
+     * @param target zip文件存放路径
+     */
+    public static void compress2ZIP(String source,final String target){
+        if(StringUtils.isBlank(source) || StringUtils.isBlank(target)){
+            logger.error("");
+            throw new IllegalArgumentException("");
+        }
+    }
     
-    public static void compress(){
+    public static void compress2AR(){
+        
         
     }
     
-    public static void unCompress(){
-
+    
+    public static void unZIP(String source,final String target){
+    
     }
+    
 }
