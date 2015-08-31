56769aa276f9f0a2f9e6b53017aa6fa3b2f8fb43
tomaer <i@tomaer.com>
2015-08-31 21:03:11 +0800

    还是eclipse用的比较习惯

---
+++ framework-common/src/main/java/com/tomaer/framework/common/utils/SpringBeanUtils.java
@@ -0,0 +1,50 @@
+package com.tomaer.framework.common.utils;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+/**
+ * Created by tomaer on 2015/8/31.
+ */
+public final class SpringBeanUtils implements BeanFactoryPostProcessor {
+
+
+    private SpringBeanUtils() {}
+
+    private static ConfigurableListableBeanFactory beanFactory;
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        SpringBeanUtils.beanFactory = beanFactory;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T getBean(String name) throws BeansException {
+        return (T) beanFactory.getBean(name);
+    }
+
+    public static <T> T getBean(Class<T> clz) throws BeansException {
+        T result = beanFactory.getBean(clz);
+        return result;
+    }
+
+    public static boolean containsBean(String name) {
+        return beanFactory.containsBean(name);
+    }
+
+    public static boolean isSingleton(String name) throws NoSuchBeanDefinitionException {
+        return beanFactory.isSingleton(name);
+    }
+
+    public static Class<?> getType(String name) throws NoSuchBeanDefinitionException {
+        return beanFactory.getType(name);
+    }
+
+    public static String[] getAliases(String name) throws NoSuchBeanDefinitionException {
+        return beanFactory.getAliases(name);
+    }
+
+
+}
